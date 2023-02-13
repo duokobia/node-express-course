832663caeb29958400b70b42f9eef955e68536f1
Daniel Okobia <duokobia@gmail.com>
2023-02-13 13:46:39 +0100

    Submission for Week6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,37 +3,28 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
-const port = 3000;
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-// A middleware built into ExpressJS is express.json(). If we dont use this,
-// we won't have that data in req.body.
+const port = process.env.PORT || 3000;
+
+// A middleware 
+app.use(express.static("./public"));
 app.use(express.json());
 
-
 // routes
-app.get("/hello", (req, res) => {
-    res.send("Task Manager App")
-});
-
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-
-// app.get("/api/v1/tasks")  - get all the tasks
-// app.post("/api/v1/tasks")  - create a new task
-// app.get("/api/v1/tasks:id")  - get single task
-// app.patch("/api/v1/tasks:id")  - update task
-// app.delete("/api/v1/tasks:id")  - delete task
-
-// So that only connection to DB is successful, then we can spin off the server
-const start = async() => {
+async function start() {
     try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening on port ${port}...`))
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
 
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 };
-// to access the dotenv anywhere, use process.env.<packageName>
 
 start()
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -7,16 +7,10 @@ const {
     getTask,
     updateTask,
     deleteTask,
+    editTask,
 } = require("../controllers/tasks");
 
-// app.get("/api/v1/tasks")  - get all the tasks
-// app.post("/api/v1/tasks")  - create a new task
-// app.get("/api/v1/tasks:id")  - get single task
-// app.patch("/api/v1/tasks:id")  - update task
-// app.delete("/api/v1/tasks:id")  - delete task
-
-
 router.route("/").get(getAllTasks).post(createTask);
-router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask).put(editTask);
 
 module.exports = router;
